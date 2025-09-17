b8f1779050d88f489309f146391ecac123df2549
Adnan <adnanali32721414114786@gmail.com>
2025-09-18 00:57:38 +0530

    Updated errors handling

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import { fileURLToPath } from "url";
 import path from "path";
 import ejsMate from "ejs-mate";
 import methodOverride from "method-override";
+import wrapAsync from "./utils/wrapAsync.js";
+import ExpressError from "./utils/ExpressError.js";
 import dotenv from "dotenv"
 dotenv.config()
 
@@ -18,7 +20,7 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/airbnb";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -37,50 +39,65 @@ async function main() {
     await mongoose.connect(MONGO_URL)
 }
 
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async(req, res) => {
     let allListings = await Listing.find({})
     res.render("listings/index.ejs", { allListings })
-})
+}))
 
 // New Route
-app.get("/listings/new", (req, res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs")
 })
 
 // Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res) => {
+    if(!req.body.listen){
+        throw new ExpressError(400, "Send valid data for listing")
+    }
     const newListing = new Listing(req.body.listing)
     await newListing.save();
     res.redirect("/listings")
-})
+}))
 
 // Show Route
-app.get("/listings/:id", async (req, res)=>{
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id)
     res.render("listings/show.ejs", { listing });
-})
+}))
 
 // Edit Route
-app.get("/listings/:id/edit", async (req, res)=>{
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing })
-})
+}))
 
 //  Update Route
-app.put("/listings/:id", async (req, res)=>{
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+    if(!req.body.listen){
+        throw new ExpressError(400, "Send valid data for listing")
+    }
     let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing})
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing })
     res.redirect(`/listings/${id}`);
-})
+}))
 
 // Delete Route
-app.delete("/listings/:id", async (req, res)=>{
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findByIdAndDelete(id);
     // console.log(listing);
     res.redirect("/listings")
+}))
+
+    app.use((req, res, next) => {
+        next(new ExpressError(404, "Page Not Found"))
+    })
+
+app.use((err, req, res, next) => {
+    let { statusCode=500, message= "Something went wrong!"} = err;
+    res.status(statusCode).send(message);
 })
 
 app.get("/", (req, res) => {
--- models/listing.js
+++ models/listing.js
@@ -14,7 +14,6 @@ const listingSchema = new Schema({
         filename: { type: String, default: "listingimage" },
         url: {
             type: String,
-            default: "https://images.unsplash.com/photo-1586810724476-c294fb7ac01b?q=80&w=436&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
     },
     price: {
